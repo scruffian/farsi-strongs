c41859c6aa63cfd2ca8200c513b7fec3c00584f9
st <99179214+borealsole@users.noreply.github.com>
2022-02-14 16:02:06 +0000

    Using extended Google translate API and comparing synsets of translation synonyms with synsets of esv words to find highest Wu-Palmer semantic similarity score (0-1). Created NMV_translations.json for Genesis 1-2 which contains array of all possible translations returned alongside the farsi word. Used python NLTK wordnet for matching words as couldn't find a node.js implementation of semantic similarity. NMV_strongs.json contains esv english words and strongs numbers matched where possible to farsi words for Genesis 1-2. match is considered when there is a similarity score of 1 between any possible translation synset and any esv word synset.

--- index.mjs
+++ index.mjs
@@ -1,17 +1,23 @@
 import { promises as fs, constants, link } from 'fs';
-import natural from 'natural';
+// import natural from 'natural';
+import translate from 'extended-google-translate-api';
+//import WordNet from 'natural/lib/natural/wordnet/wordnet';
+// import * as use from '@tensorflow-models/universal-sentence-encoder';
+// import child_process from 'child_process';
+// import {PythonShell} from 'python-shell';
 
 // Imports the Google Cloud client library
-import { v2 } from '@google-cloud/translate';
-const { Translate } = v2;
+/* import { v2 } from '@google-cloud/translate';
+const { Translate } = v2; */
 
 // Creates a client
-const translate = new Translate({
+/* const translate = new Translate({
     projectId: 'cedar-hawk-340415', //eg my-proj-0o0o0o0o'
     keyFilename: 'cedar-hawk-340415-c7ae4e5b7fe4.json' //eg my-proj-0fwewexyz.json
-});
+}); */
 
 (async function start() {
+	// const wordnet = new natural.WordNet()
 	const esv = await fs.readFile( 'ESV.json', 'utf8' );
 	const JsonESV = JSON.parse( esv );
 	const ESVBooks = JsonESV.books;
@@ -28,41 +34,128 @@ const translate = new Translate({
 	const nmv = await fs.readFile( 'NMV.json' );
 	const JsonNMV = JSON.parse( nmv );
 	const NMVBooks = JsonNMV.books;
-	const verse = NMVBooks['Genesis'][0][0].split( ' ' );
+	// const verse = NMVBooks['Genesis'][0][0];
+	// const book = 'Genesis'
+	const outjson = {books:{}}
 
-	/*Object.keys( NMVBooks ).forEach( book => {
-		NMVBooks[ book ].forEach( chapter => {
-			chapter.forEach( verse => {
-				verse.split( ' ' ).forEach( word => {
-					console.log( word );
-				} );
-			} );
-		})
-	});*/
-	natural.PorterStemmer.attach();
-	const translatedVerse = await googleTranslateText( verse );
-	translatedVerse.forEach( translatedWord => {
+	for (const book of Object.keys( NMVBooks )) {
+		// console.log(book)
+		outjson.books[book]=[]
+		var nChapter = 0
+
+		for (const chapter of NMVBooks[ book ]) {
+			// console.log(nChapter + 1)
+			outjson.books[book].push([])
+			var nVerse = 0
+			const verse = NMVBooks[book][nChapter][nVerse]
+
+			for (const verse of chapter) {
+				console.log([book, nChapter +1, nVerse + 1])
+				outjson.books[book][nChapter].push([])
+				for (const faWord of verse.split( ' ' )) {
+					const res = await extendedTranslateText( faWord );
+					const translations = []
+					if (res['translations'] instanceof Object){
+						for (const wordType of Object.keys(res['translations'])) {
+							for (const translationChoice of res['translations'][wordType]) {
+								translations.push(translationChoice);
+							};
+						};
+						//console.log(res['word'], translations);
+					}
+					else if (res['translations'] instanceof Array) {
+						translations.push(res['translations']);
+					};
+					
+					outjson.books[book][nChapter][nVerse].push([faWord, translations])
+
+
+					/* const similarities = []
+					for (const englishWord of ESVBooks['Genesis'][0][0]) {
+						if ( englishWord[1] ) {
+							for (const translatedWord of translations) {
+								const options = {
+									mode: 'text',
+									pythonPath: '...\\python.exe',
+									pythonOptions: ['-u'], // get print results in real-time
+									scriptPath: '...\\farsi-strongs',
+									args: [translatedWord, englishWord[0]]
+								};
+								
+								PythonShell.run('wupsimilarity.py', options, function (err, results) {
+									if (err) throw err;
+									// results is an array consisting of messages collected during execution
+									similarities.push(
+										{
+											similarity: results, 
+											translation:translatedWord, 
+											ESVword:englishWord[0], 
+											strongs:englishWord[1]
+										}
+									);
+								});
+								if (translatedWord == englishWord[0]) {
+									console.log([faWord, translatedWord, englishWord])
+								}	
+							}; 
+								
+						};
+					}; */
+				};
+				nVerse++
+				// console.log({farsi:faWord, similarities:similarities})
+			}
+			nChapter++
+			fs.writeFile("NMV_translations.json", JSON.stringify(outjson))
+		} ;
+	}
+	
+}
+	
+	// const faWord = verse[1]
+		
+
+
+			// const faWordTranslations = { faWord: translations}
+
+			/* translations.forEach(translatedWord => {
+				wordnet.lookup(translatedWord, function(details) {
+					details.forEach(function(detail) {
+						console.log([res['word'], translatedWord, detail.synonyms])
+					});
+				});
+
+			}); */
+	
+	
+	/* translatedVerse.forEach( translatedWord => {
 		ESVBooks['Genesis'][0][0].forEach( englishWord => {
 			if ( englishWord[1] ) {
-				const esvWord = englishWord[0].toLowerCase();
-				const farsiWord = translatedWord.toLowerCase();
+				
+				const esvWord = natural.PorterStemmer.stem(englishWord[0]);
+				if (' ' in translatedWord) {
+					const farsiWord = natural.PorterStemmer.attach(translatedWord.tokenizeAndStem());
+					//console.log(tokenizer.tokenize("my dog hasn't any fleas."));
+					//const farsiWordArr = tokenizer.tokenize(translatedWord).stem();
+				}
+				else {
+					const farsiWord = natural.PorterStemmer.stem(translatedWord);
+				}
 				//console.log( esvWord, farsiWord );
 
 				//console.log("I can see that we are going to be friends".tokenizeAndStem())
-				console.log( esvWord.tokenizeAndStem() );
-				if ( esvWord.indexOf( farsiWord ) > -1 ) {
-					console.log( englishWord[0] );
+				//console.log( esvWord.tokenizeAndStem() );
+				if ( esvWord == farsiWord ) {
+					console.log( englishWord.slice(0,2)  );
 				}
 			}
  		} )
-	} )
+	} ) */
+
+)();
 
 
-
-})();
-
-
-async function googleTranslateText( text ) {
+/* async function googleTranslateText( text ) {
 	const target = 'en';
 
 	// Translates the text into the target language. "text" can be a string for
@@ -73,7 +166,34 @@ async function googleTranslateText( text ) {
 	translations = Array.isArray(translations) ? translations : [translations];
 
 	return translations;
-	/*translations.forEach((translation, i) => {
+	translations.forEach((translation, i) => {
 		console.log(`${text[i]} => (${target}) ${translation}`);
-	});*/
+	});
+} */
+async function extendedTranslateText (text) {
+	const res = translate(text, "fa", "en", {detailedTranslationsSynonyms: false});
+return res;
 }
+
+/* function semanticSimilarity (string1, string2) {
+	use.load().then(model => {
+		const sentences = [
+			string1,
+			string2
+		];
+		model.embed(sentences).then(embeddings => {
+			const a = embeddings[0]
+			const b = embeddings[1]
+			const magnitudeA = Math.sqrt(this.dot(a, a));
+			const magnitudeB = Math.sqrt(this.dot(b, b));
+			if (magnitudeA && magnitudeB){
+				return this.dot(a, b) / (magnitudeA * magnitudeB);
+			}
+			else {
+				return NaN
+			};
+		});
+	});
+  
+	
+} */
---
+++ wupsimilarity.py
@@ -0,0 +1,125 @@
+
+from nltk.corpus import wordnet
+import json
+import pandas as pd
+from copy import deepcopy
+
+with open("NMV_translations.json", encoding="utf8") as f:
+    nmv_translations = json.load(f)
+
+with open("ESV.json") as f:
+    esv = json.load(f)
+
+def SemanticSimilarity():
+    for book in nmv_translations["books"]:
+        for chapter, n_chapter in zip(
+            nmv_translations["books"][book], 
+            range(0,len(nmv_translations["books"][book]))
+        ):
+            for verse, n_verse in zip(
+                chapter, 
+                range(0,len(chapter))
+            ):
+                for word, n_word in zip(
+                    verse, 
+                    range(0, len(verse))
+                ):
+                    if len(word[1]) > 0:
+                        for translated_word in word[1]:
+                            translated_word_synsets = wordnet.synsets(translated_word)
+
+                            for esv_word in esv["books"][book][n_chapter][n_verse]:
+                                esv_word_synsets = wordnet.synsets(esv_word[0])
+                                similarities = []
+                                if translated_word_synsets and esv_word_synsets:
+                                    for t_synset in translated_word_synsets:
+                                        for e_synset in esv_word_synsets:
+                                            s = t_synset.wup_similarity(e_synset)
+                                            similarities.append(
+                                                s
+                                            )
+                                            # print(f"farsi: {word[0]}, translation: {translated_word}, esv: {esv_word}, similarity: {s}")
+                                            # print(translated_word_synsets)
+                                            # print(esv_word)
+                                            # print(esv_word_synsets)
+                                    esv_word_links = pd.DataFrame(
+                                        {
+                                            "book":[book],
+                                            "chapter":[n_chapter],
+                                            "verse":[n_verse],
+                                            "word_order":[n_word],
+                                            "farsi_word":[word[0]],
+                                            "possible_translation":[translated_word],
+                                            "esv_word": [esv_word[0]], 
+                                            "esv_strongs": [
+                                                esv_word[1] if len(esv_word) > 1 else None
+                                            ],
+                                            "esv_morphology": [
+                                                esv_word[2] if len(esv_word) > 2 else None
+                                            ],
+                                            "max_similarity":[max(similarities)]
+                                        }
+                                    )
+                                    yield esv_word_links
+                                else:
+                                    esv_word_links = pd.DataFrame(
+                                        {
+                                            "book":[book],
+                                            "chapter":[n_chapter],
+                                            "verse":[n_verse],
+                                            "word_order":[n_word],
+                                            "farsi_word":[word[0]],
+                                            "possible_translation":[None],
+                                            "esv_word": [None], 
+                                            "esv_strongs": [None],
+                                            "esv_morphology": [None],
+                                            "max_similarity":[0]
+                                        }
+                                    )
+                                    yield esv_word_links
+                    else:
+                        esv_word_links = pd.DataFrame(
+                            {
+                                "book":[book],
+                                "chapter":[n_chapter],
+                                "verse":[n_verse],
+                                "word_order":[n_word],
+                                "farsi_word":[word[0]],
+                                "possible_translation":[None],
+                                "esv_word": [None], 
+                                "esv_strongs": [None],
+                                "esv_morphology": [None],
+                                "max_similarity":[0]
+                            }
+                        )
+                        yield esv_word_links
+                        
+similarity_df = pd.concat(SemanticSimilarity()).set_index(["book","chapter","verse","farsi_word"])                                    
+similarity_max_df = (
+    similarity_df
+    .sort_values(
+        by="max_similarity"
+    )
+    .reset_index()
+    .drop_duplicates(
+        subset=["book","chapter","verse","farsi_word"], 
+        keep="last"
+    )
+)
+
+out_json = deepcopy(nmv_translations)
+for book in out_json["books"]:
+    for chapter in range(0, len(out_json["books"][book])):
+        for verse in range(0, len(out_json["books"][book][chapter])):
+            for word in range(0, len(out_json["books"][book][chapter][verse])):
+                if len(out_json["books"][book][chapter][verse][word]) > 1:
+                    out_json["books"][book][chapter][verse][word].pop(1)
+for i, row in similarity_max_df.iterrows():
+    if row["max_similarity"] == 1:
+        data = [row["esv_word"], row["esv_strongs"]]
+        out_json["books"][row["book"]][row["chapter"]][row["verse"]][row["word_order"]].extend(data)
+
+# out_data = [[row["farsi_word"], row["esv_word"], row["esv_strongs"]] if row["max_similarity"] ==1 else [row["farsi_word"]] for i, row in similarity_max_df.iterrows()]
+with open("NMV_strongs.json","w",encoding="utf8") as out_f:
+    json.dump(out_json, out_f, ensure_ascii=False)
+
