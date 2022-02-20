f1be2a64a4608fa29b81e5d6a15379615ae2e23f
st <99179214+borealsole@users.noreply.github.com>
2022-02-20 20:47:58 +0000

    check for previously started output file and add if statements to pick up progress from last time if previously hit API call limit

--- index.mjs
+++ index.mjs
@@ -31,64 +31,85 @@ const { Translate } = v2; */
 	const NMVBooks = JsonNMV.books;
 	// const verse = NMVBooks['Genesis'][0][0];
 	// const book = 'Genesis'
-	const outjson = {books:{}};
+	if (await checkFileExists('NMV_translations_'+book+'.json')) {
+		const existingOutputJSON = await fs.readFile('NMV_translations_'+book+'.json');
+		var outjson = JSON.parse(existingOutputJSON);
+		var lastWrittenChapterIndex = outjson.books[book].length - 1;
+		var lastWrittenVerseIndex = outjson.books[book][nChapter].length - 1;
+	}
+	else {
+		var outjson = {books:{}};
+		outjson.books[book]=[];
+		var lastWrittenChapterIndex = -1;
+		var lastWrittenVerseIndex = -1;
+	};
 
-	outjson.books[book]=[];
-	var nChapter = 0;
-	
-	for (const chapter of NMVBooks[ book ]) {
-		// console.log(nChapter + 1)
-		outjson.books[book].push([]);
-		var nVerse = 0;
-		const verse = NMVBooks[book][nChapter][nVerse];
-
-		for (const verse of chapter) {
-			console.log([book, nChapter +1, nVerse + 1]);
-			const splitVerse = verse.split(' ');
-			const verseWordPromises = splitVerse.map(async faWord => {
-				const wordResult = await translate(faWord, "fa", "en").then(res => {
-					//console.log(res);
-					const translations = [];
-					if (res['translations'] instanceof Object){
-						for (const wordType of Object.keys(res['translations'])) {
-							for (const translationChoiceObj of res['translations'][wordType]){
-								translations.push(translationChoiceObj);
+	for (const [chapterIndex, chapter] of NMVBooks[ book ].entries()) {
+		if (
+			(chapterIndex > lastWrittenChapterIndex) || 
+			(chapterIndex = lastWrittenChapterIndex && lastWrittenVerseIndex < (chapter.length - 1))
+		) {
+			// console.log(nChapter + 1);
+			if (chapterIndex > lastWrittenChapterIndex) {
+				outjson.books[book].push([]);
+				// var nVerse = 0;
+			};
+			for (const [verseIndex, verse] of chapter.entries()) {
+				if (verseIndex > lastWrittenVerseIndex) {
+					console.log([book, chapterIndex +1, verseIndex + 1]);
+					const splitVerse = verse.split(' ');
+					const verseWordPromises = splitVerse.map(async faWord => {
+						const wordResult = await translate(faWord, "fa", "en").then(res => {
+							//console.log(res);
+							const translations = [];
+							if (res['translations'] instanceof Object){
+								for (const wordType of Object.keys(res['translations'])) {
+									for (const translationChoiceObj of res['translations'][wordType]){
+										translations.push(translationChoiceObj);
+									};
+								};
+							}
+							else if (res['translations'] instanceof Array) {
+								for (const translationChoiceArr of res['translations']){
+									translations.push(translationChoiceArr);
+								};
 							};
-						};
-					}
-					else if (res['translations'] instanceof Array) {
-						for (const translationChoiceArr of res['translations']){
-							translations.push(translationChoiceArr);
+							return [faWord, translations];
+						});
+						
+						return wordResult;
+					});
+
+					try {
+						const verseResult = await Promise.all(verseWordPromises)
+						// console.log(verseResult);
+						outjson.books[book][nChapter].push(verseResult);
+					} catch (error) {
+						if (error['message'] == 'HTTPError') {
+							console.log('API call limit reached');
+							await fs.writeFile('NMV_translations_'+book+'.json', JSON.stringify(outjson));
+							exit(0);
 						};
 					};
-					return [faWord, translations];
-				});
-				
-				return wordResult;
-			});
-
-			try {
-				const verseResult = await Promise.all(verseWordPromises)
-				// console.log(verseResult)
-				outjson.books[book][nChapter].push(verseResult);
-			} catch (error) {
-				if (error['message'] == 'HTTPError') {
-					console.log('API call limit reached');
-					await fs.writeFile('NMV_translations_'+book+'.json', JSON.stringify(outjson));
-					exit(0);
-				}
+				};
 			};
-
-			nVerse++;
 		};
-
-		nChapter++;
 	};
 		
 	await fs.writeFile("NMV_translations_"+book+".json", JSON.stringify(outjson));
 })();
 
+async function checkFileExists(file) {
+	try {
+		const result = await fs.access(file).then(() => true);
 
+		return result;
+	} catch {
+		const result = false;
+
+		return result;
+	};
+  };
 	// const wordnet = new natural.WordNet()
 	// const esv = await fs.readFile( 'ESV.json', 'utf8' );
 	// const JsonESV = JSON.parse( esv );
