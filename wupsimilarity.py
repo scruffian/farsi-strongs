d2ebb4d93601cc2c55aa186e9b4e5447ca999878
st <99179214+borealsole@users.noreply.github.com>
2022-04-21 11:40:06 +0100

    Add ability to specify version to generate strongs

--- wupsimilarity.py
+++ wupsimilarity.py
@@ -7,12 +7,13 @@ def FindSimilarWords():
     from copy import deepcopy
     
     book_name = sys.argv[1]
+    eng_version = sys.argv[2]
 
     with open(f"transformations/NMV_translations_{book_name}.json", encoding="utf8") as f:
         nmv_translations = json.load(f)
 
-    with open("inputs/ESV.json") as f:
-        esv = json.load(f)
+    with open(f"inputs/{eng_version}.json") as f:
+        eng = json.load(f)
 
     def SemanticSimilarity():
         for book in nmv_translations["books"]:
@@ -33,23 +34,23 @@ def FindSimilarWords():
                             for translated_word in word[1]:
                                 translated_word_synsets = wordnet.synsets(translated_word)
 
-                                for esv_word in esv["books"][book][n_chapter][n_verse]:
-                                    esv_word_synsets = wordnet.synsets(esv_word[0])
+                                for eng_word in eng["books"][book][n_chapter][n_verse]:
+                                    eng_word_synsets = wordnet.synsets(eng_word[0])
                                     # TODO - look at options for speeding up... there's a function for getting every possible list pair I think
                                     # Can more generators be worked in? or Recursion? Vectorise? Need less nested iteration.
                                     similarities = []
-                                    if translated_word_synsets and esv_word_synsets:
+                                    if translated_word_synsets and eng_word_synsets:
                                         for t_synset in translated_word_synsets:
-                                            for e_synset in esv_word_synsets:
+                                            for e_synset in eng_word_synsets:
                                                 s = t_synset.wup_similarity(e_synset)
                                                 similarities.append(
                                                     s
                                                 )
-                                                # print(f"farsi: {word[0]}, translation: {translated_word}, esv: {esv_word}, similarity: {s}")
+                                                # print(f"farsi: {word[0]}, translation: {translated_word}, eng: {eng_word}, similarity: {s}")
                                                 # print(translated_word_synsets)
-                                                # print(esv_word)
-                                                # print(esv_word_synsets)
-                                        esv_word_links = pd.DataFrame(
+                                                # print(eng_word)
+                                                # print(eng_word_synsets)
+                                        eng_word_links = pd.DataFrame(
                                             {
                                                 "book":[book],
                                                 "chapter":[n_chapter],
@@ -57,19 +58,19 @@ def FindSimilarWords():
                                                 "word_order":[n_word],
                                                 "farsi_word":[word[0]],
                                                 "possible_translation":[translated_word],
-                                                "esv_word": [esv_word[0]], 
-                                                "esv_strongs": [
-                                                    esv_word[1] if len(esv_word) > 1 else None
+                                                "eng_word": [eng_word[0]], 
+                                                "eng_strongs": [
+                                                    eng_word[1] if len(eng_word) > 1 else None
                                                 ],
-                                                "esv_morphology": [
-                                                    esv_word[2] if len(esv_word) > 2 else None
+                                                "eng_morphology": [
+                                                    eng_word[2] if len(eng_word) > 2 else None
                                                 ],
                                                 "max_similarity":[max([s if s else 0 for s in similarities]) if len(similarities) > 0 else 0]
                                             }
                                         )
-                                        yield esv_word_links
+                                        yield eng_word_links
                                     else:
-                                        esv_word_links = pd.DataFrame(
+                                        eng_word_links = pd.DataFrame(
                                             {
                                                 "book":[book],
                                                 "chapter":[n_chapter],
@@ -77,15 +78,15 @@ def FindSimilarWords():
                                                 "word_order":[n_word],
                                                 "farsi_word":[word[0]],
                                                 "possible_translation":[None],
-                                                "esv_word": [None], 
-                                                "esv_strongs": [None],
-                                                "esv_morphology": [None],
+                                                "eng_word": [None], 
+                                                "eng_strongs": [None],
+                                                "eng_morphology": [None],
                                                 "max_similarity":[0]
                                             }
                                         )
-                                        yield esv_word_links
+                                        yield eng_word_links
                         else:
-                            esv_word_links = pd.DataFrame(
+                            eng_word_links = pd.DataFrame(
                                 {
                                     "book":[book],
                                     "chapter":[n_chapter],
@@ -93,13 +94,13 @@ def FindSimilarWords():
                                     "word_order":[n_word],
                                     "farsi_word":[word[0]],
                                     "possible_translation":[None],
-                                    "esv_word": [None], 
-                                    "esv_strongs": [None],
-                                    "esv_morphology": [None],
+                                    "eng_word": [None], 
+                                    "eng_strongs": [None],
+                                    "eng_morphology": [None],
                                     "max_similarity":[0]
                                 }
                             )
-                            yield esv_word_links
+                            yield eng_word_links
                             
     similarity_df = pd.concat(SemanticSimilarity()).set_index(["book","chapter","verse","farsi_word"])                                    
     similarity_max_df = (
@@ -122,16 +123,19 @@ def FindSimilarWords():
                     if len(out_json["books"][book][chapter][verse][word]) > 1:
                         out_json["books"][book][chapter][verse][word].pop(1)
     for i, row in similarity_max_df.iterrows():
-        if row["max_similarity"] == 1 and type(row["esv_strongs"]==str):
-            # data = [row["esv_word"], row["esv_strongs"]]
-            out_json["books"][row["book"]][row["chapter"]][row["verse"]][row["word_order"]].append(row["esv_strongs"])
+        if row["max_similarity"] == 1 and type(row["eng_strongs"]==str):
+            # data = [row["eng_word"], row["eng_strongs"]]
+            out_json["books"][row["book"]][row["chapter"]][row["verse"]][row["word_order"]].append(row["eng_strongs"])
 
-    with open(f"transformations/NMV_strongs_{book_name}.json","w",encoding="utf8") as out_f:
+    with open(f"transformations/NMV_{eng_version}_strongs_{book_name}.json","w",encoding="utf8") as out_f:
         json.dump(out_json, out_f, ensure_ascii=False)
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print('Please specify a book')
+        sys.exit()
+    elif len(sys.argv) <= 2:
+        print('Please specify an english version')
         sys.exit()
     else: 
         FindSimilarWords()
