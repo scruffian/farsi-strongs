c4e4a09946fa5a1e113ea222ab37b30c3a083951
st <99179214+borealsole@users.noreply.github.com>
2022-10-06 22:25:38 +0100

    Add batch api call function

--- unique_word_translator.mjs
+++ unique_word_translator.mjs
@@ -1,31 +1,47 @@
 import { promises as fs, constants, link } from 'fs';
 import translate from 'google-translate-extended-api';
-import {exit } from 'process';
-
+import { exit } from 'process';
 
 (async function start() {
-
+	// Read list of unique farsi words from keys of input file
 	const nmv = await fs.readFile( 'keyed-words.json' );
 	var keyed_words = JSON.parse( nmv );
 	keyed_words = Object.keys(keyed_words);
-	keyed_words = keyed_words.slice(15);
 
+	// Set chunk size for async api calls
+	const chunksize = 20
+
+	// Remove punctuation at start of words array
+	keyed_words = keyed_words.slice(15);
+	const iterations = Math.ceil(keyed_words.length / chunksize);
+
+	// Pick up from where last run left off if previously run
 	if (await checkFileExists('transformations/keyed_words_translations.json')) {
 		const existingOutputJSON = await fs.readFile('transformations/keyed_words_translations.json');
 		var outjson = JSON.parse(existingOutputJSON);
-		keyed_words = keyed_words.slice(outjson.length);
+		keyed_words = keyed_words.slice(Object.keys(outjson).length+1);
 	}
 	else {
 		var outjson = {};
 	};
 
-	for (const [idx, faword] of keyed_words.entries()) {
-		console.log(faword);
+	// Iterate through chunks of unique words and pass to translate api
+	for (var i=0; i < iterations; i++){
+		var chunk = keyed_words.splice(0,chunksize);
+		var chunkPromises = chunk.map(async faWord => {
+			var wordResult = await translate(faWord, "fa", "en");
+			return wordResult;
+		});
+		// Append translation results to outjson
 		try {
-            
-            const wordResult = await translate(faword, "fa", "en");
-            outjson[faword] = wordResult;
-        } catch (error) {
+            var results = await Promise.all(chunkPromises);
+			results.forEach(obj => {
+				outjson[obj['word']] = obj;
+			});
+        } 
+
+		// Write outjson with appended results so far to file on error
+		catch (error) {
             if (error['message'] == 'HTTPError') {
                 console.log('API call limit reached');
                 await fs.writeFile('transformations/keyed_words_translations.json', JSON.stringify(outjson));
@@ -33,8 +49,14 @@ import {exit } from 'process';
             }
 			else {
 				console.log(error)
+				await fs.writeFile('transformations/keyed_words_translations.json', JSON.stringify(outjson));
+				exit(0);
 			};
         };
+		var progress = (i/iterations)*100;
+		progress = progress.toFixed(2);
+		console.clear();
+		console.log('\r' + progress + "% complete");
     };	
 	await fs.writeFile("transformations/keyed_words_translations.json", JSON.stringify(outjson));
 })();
