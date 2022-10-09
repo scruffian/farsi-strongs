192908adcc35b417a1a4c2af2e2dd088c9d49d1f
st <99179214+borealsole@users.noreply.github.com>
2022-10-09 21:14:55 +0100

    Fix console output to show real progress

--- unique_word_translator.mjs
+++ unique_word_translator.mjs
@@ -1,6 +1,5 @@
 import { promises as fs, constants, link } from 'fs';
 import translate from 'google-translate-extended-api';
-import { exit } from 'process';
 
 (async function start() {
 	// Read list of unique farsi words from keys of input file
@@ -20,16 +19,18 @@ import { exit } from 'process';
 		const existingOutputJSON = await fs.readFile('transformations/keyed_words_translations.json');
 		var outjson = JSON.parse(existingOutputJSON);
 		keyed_words = keyed_words.slice(Object.keys(outjson).length+1);
+		var iterations_run = iterations - Math.ceil(keyed_words.length / chunksize);
 	}
 	else {
 		var outjson = {};
+		var iterations_run = 0;
 	};
 
 	// Iterate through chunks of unique words and pass to translate api
-	for (var i=0; i < iterations; i++){
+	for (var i = iterations_run; i < iterations; i++){
 		var chunk = keyed_words.splice(0,chunksize);
 		var chunkPromises = chunk.map(async faWord => {
-			var wordResult = await translate(faWord, "fa", "en");
+			var wordResult = await translate(faWord, 'fa', 'en');
 			return wordResult;
 		});
 		// Append translation results to outjson
@@ -43,22 +44,23 @@ import { exit } from 'process';
 		// Write outjson with appended results so far to file on error
 		catch (error) {
             if (error['message'] == 'HTTPError') {
-                console.log('API call limit reached');
+                console.log('/nAPI call limit reached');
                 await fs.writeFile('transformations/keyed_words_translations.json', JSON.stringify(outjson, null, 4));
-                exit(0);
+                process.exit(0);
             }
 			else {
-				console.log(error)
+				console.log('');
+				console.log(error);
 				await fs.writeFile('transformations/keyed_words_translations.json', JSON.stringify(outjson, null, 4));
-				exit(0);
+				process.exit(0);
 			};
         };
 		var progress = (i/iterations)*100;
 		progress = progress.toFixed(2);
-		console.clear();
-		console.log('\r' + progress + "% complete");
+		process.stdout.write('\r' + progress + '% complete');
     };
-	await fs.writeFile("transformations/keyed_words_translations.json", JSON.stringify(outjson, null, 4));
+	process.stdout.write('\r100%   complete');
+	await fs.writeFile('transformations/keyed_words_translations.json', JSON.stringify(outjson, null, 4));
 })();
 
 async function checkFileExists(file) {
